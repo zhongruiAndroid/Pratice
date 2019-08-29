d5c534504037520b14862fd8e890461534ce63c6
windows10 <2380253499@qq.com>
2019-08-29 23:17:34 +0800

    file

--- app/src/main/java/com/test/pratice/MainActivity.java
+++ app/src/main/java/com/test/pratice/MainActivity.java
@@ -6,19 +6,24 @@ import android.util.Log;
 import com.test.pratice.bean.Book;
 import com.test.pratice.eventbus.MyBus;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EventBus.getDefault().register(this);
         setContentView(R.layout.activity_main);
 
         MyBus.get().register(this);
 
         MyBus.get().post(new Book());
 
-//        EventBus.getDefault().post(new Book());
+
+
+        EventBus.getDefault().register(this);
+        EventBus.getDefault().post(new Book());
+        EventBus.getDefault().postSticky(new Book());
     }
     @com.test.pratice.eventbus.Subscribe(threadMode= com.test.pratice.eventbus.ThreadMode.MAIN)
     public void onEven1t(Book book){
